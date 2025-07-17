fb44e39317e85a2541f14f43a8c633e9b04e2aff
Rachael Sewell <rachmari@github.com>
2025-07-17 12:27:53 -0700

    Fix duplicate definition of HELLO_WORLD_2 in defines.h

--- defines.h
+++ defines.h
@@ -1,11 +1,14 @@
 #ifndef _DEFINE_1
 #define _DEFINE_1
+
 #define DEFINE_2
 
 #define DEFINE_3
 
 #define HELLO_WORLD_1 1		
+
 #define HELLO_WORLD_2 2		
 
 #define HELLO_WORLD_2 2
+
 #define HELLO_WORLD_2 3
