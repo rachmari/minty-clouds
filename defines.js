0136b1782c0fbf1804646a41a82c0d3f7e6b3b06
Rachael Sewell <rachmari@github.com>
2025-07-17 12:19:22 -0700

    Update defines.js

--- defines.js
+++ defines.js
@@ -2,3 +2,16 @@ const HELLO = 'world'
 const WORLD = 'hello'
 
 const MYNAME = 'Rachael'
+export const PI = 3.14159;
+
+// Exporting a function
+export function square(x) {
+    return x * x;
+}
+
+// Exporting a class
+export class Utils {
+    printHello() {
+        console.log("Hello!");
+    }
+}
